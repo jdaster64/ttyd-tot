fcd7926ce11e0a78bd2f7b5b8b45895150853762
jdaster64 <jonathan.d.aldrich1@gmail.com>
2024-03-29 19:53:06 -0400

    Title update

--- ttyd-tools/rel/source/mod_title.cpp
+++ ttyd-tools/rel/source/mod_title.cpp
@@ -14,7 +14,7 @@ namespace mod::infinite_pit {
 
 namespace {
 
-constexpr const char* kTitleInfo = "Project ToT alpha r3 2024-03-28";
+constexpr const char* kTitleInfo = "Project ToT alpha r4 2024-03-29";
     
 // constexpr const char* kTitleInfo =
     // "PM:TTYD Infinite Pit v2.40 r64 by jdaster64\n"
