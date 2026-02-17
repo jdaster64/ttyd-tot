15c64bb1723dcd690deb8fa230d5f23f864bf65f
jdaster64 <jonathan.d.aldrich1@gmail.com>
2026-02-17 14:10:49 -0500

    Iron Cleft hitpos fix, battle_detect defs

---
+++ ttyd-tools/rel/include/ttyd/battle_detect.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <cstdint>
+
+namespace ttyd::battle {
+struct BattleWorkWeaponTargets;
+}
+
+namespace ttyd::battle_detect {
+
+extern "C" {
+
+// BattleGetFirstAttackUnit
+// BattleChoiceSamplingEnemy
+// BattleSamplingEnemyUpdate
+// BattleSamplingEnemy
+void _btlSamplingEnemy(BattleWorkWeaponTargets* twork);
+
+}
+
+}
